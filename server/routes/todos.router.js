d2a38d8a0abc42cfbeeede3134c36878e0e6add1
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-17 11:59:19 -0500

    successfully created GET request tested using postman

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool');
 // setup get route for /todos
 router.get('/', (req,res) => {
     console.log(`In GET route /todos`);
-    const queryText = `SELECT * FROM "todos" ORDER BY "priority";`;
+    const queryText = `SELECT * FROM "todos" ORDER BY "id";`;
     pool.query(queryText).then((result) => {
         res.send(result.rows);
     }).catch((error) => {
