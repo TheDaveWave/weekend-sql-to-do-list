3f378be191169493d41f6bc5d7ed720e254609c5
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-17 08:23:15 -0500

    put sql statements into database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "todos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"task" VARCHAR(250) NOT NULL, 
+	"due" date,
+	"isDone" BOOLEAN DEFAULT FALSE, 
+	"finished" date
+);
+
+INSERT INTO "todos" ("task", "due")
+VALUES ('Finish weekend challenge', '9-19-2020'), ('Have Fun', '1-1-2049'), ('Do a backflip', '1-1-2077');
