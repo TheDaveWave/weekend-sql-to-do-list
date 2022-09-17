5e3e7c18f0f5845f706a0c85d27ae33e8b8e365f
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-17 09:58:33 -0500

    added an updated table to database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,11 @@
 CREATE TABLE "todos" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"task" VARCHAR(250) NOT NULL, 
+	"task" VARCHAR(250) NOT NULL,
+	"priority" INTEGER DEFAULT 1 CHECK ("priority" > 0 AND "priority" < 6), --Default to highest priority of 1.
 	"due" date,
 	"isDone" BOOLEAN DEFAULT FALSE, 
 	"finished" date
 );
 
 INSERT INTO "todos" ("task", "due")
-VALUES ('Finish weekend challenge', '9-19-2022'), ('Have Fun', '1-1-2049'), ('Do a backflip', '1-1-2077');
+VALUES ('Finish weekend challenge', '9-19-2020'), ('Have Fun', '1-1-2049'), ('Do a backflip', '1-1-2077');
