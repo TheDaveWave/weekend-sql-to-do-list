117f510f078330afb6103e4b7314c2d33ef52c2b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-17 11:52:37 -0500

    setup GET route in the router.js file

---
+++ server/routes/todos.router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+// import pool config
+const pool = require('../modules/pool');
+
+// setup get route for /todos
+router.get('/', (req,res) => {
+    console.log(`In GET route /todos`);
+    const queryText = `SELECT * FROM "todos" ORDER BY "priority";`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error getting to-do's`,error);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
